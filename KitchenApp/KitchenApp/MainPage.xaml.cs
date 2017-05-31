766cd3ee2c03d972ae08152ade4f37e78923603f
Mathias Dahl Thomsen <Mathias Dahl Thomsen>
2017-05-31 08:52:25 +0200

    Add project files.

---
+++ KitchenApp/KitchenApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KitchenApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            theButton.Clicked += (sender, args) =>
+            {
+                theButton.Text = "Touched me!";
+            };
+
+            this.Content = theButton;
+              
+        }
+    }
+}
